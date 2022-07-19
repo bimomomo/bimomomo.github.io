4f4d385619d25cc63a4b71148d3f5e0fb95ac14a
bimo-a-f <bimofadli39@gmail.com>
2022-07-19 11:39:23 +0700

    recog

--- facereg/js/faceSystem.js
+++ facereg/js/faceSystem.js
@@ -4,7 +4,25 @@ $(document).ready(function(){
 const imgRcg = null;
 async function face() {
 	await loadmodel();
-	await runAi();
+
+	$('#desc').html('detecting refimg')
+	const labels = ['refimg'];
+	const labeledFaceDescriptors = await Promise.all(
+		labels.map(async label => {
+		const imgUrl = 'img/'+label+'.jpg'
+		const img = await faceapi.fetchImage(imgUrl)
+		
+		const faceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+		if (!faceDescription) {
+			throw new Error('no faces detected for '+label);
+		}
+		
+		const faceDescriptors = [faceDescription.descriptor]
+		return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
+		})
+	);
+	
+	await runAi(labeledFaceDescriptors);
 }
 async function loadmodel(){
 	$('#desc').html('start loading model')
@@ -19,10 +37,18 @@ async function loadmodel(){
 	
 	$('#desc').html('finish loading model')
 }
-async function runAi(){
+async function runAi(m){
 	$('#desc').html('start detecting face')
-	const img= document.getElementById('originalImg')
+	const img = document.getElementById('originalImg')
 	let faceDescriptions = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors().withFaceExpressions()
+	const threshold = 0.3
+	const faceMatcher = new faceapi.FaceMatcher(m, threshold)
+	if (faceDescriptions) {
+		const bm = faceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
+		console.log(bm)
+		$('#matchrate').html(bm.toString())
+	}
+
 	const canvas = $('#reflay').get(0)
 	faceapi.matchDimensions(canvas, img)
 	faceDescriptions = faceapi.resizeResults(faceDescriptions, img)
@@ -31,5 +57,5 @@ async function runAi(){
 	faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
 	$('#desc').html('finish detecting face')
 
-	await setTimeout(async () => await this.runAi(),90)
+	await setTimeout(async () => await this.runAi(m),90)
 }
